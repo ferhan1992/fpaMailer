2d0a21aa7788fc7381b009ac72c0ecbad7c1f00c
Ferhan Kaplanseren <>
2016-04-28 13:41:14 +0200

    Initialize of Git

---
+++ src/de/bht/fpa/mail/s819191/model/data/FileElement.java
@@ -0,0 +1,22 @@
+
+import java.io.File;
+import java.util.List;
+
+/*
+ * This is the leaf part of a composite pattern.
+ * 
+ * @author Simone Strippgen
+ */
+
+public class FileElement extends Component {
+
+    public FileElement(File path) {
+        super(path);
+    }
+
+    @Override
+    public boolean isExpandable() {
+        return false;
+    }
+    
+}
---
+++ src/de/bht/fpa/mail/s819191/model/data/Folder.java
@@ -0,0 +1,36 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author Simone Strippgen
+ */
+public class Folder extends Component {
+
+    private ArrayList<Component> content;
+    private boolean expandable;
+    
+    public Folder(File path, boolean expandable) {
+        super(path);
+        content = new ArrayList<Component>();
+	this.expandable = expandable;
+    }
+
+    @Override
+    public void addComponent(Component comp) {
+        content.add(comp);
+    }
+
+    @Override
+    public List<Component> getComponents() {
+        return content;
+    }
+
+    @Override
+    public boolean isExpandable() {
+        return expandable;
+    }
+    
+}
