7b273e9631893165fc2a53dd6e5035ed3356c2ef
Ferhan Kaplanseren <Admin@FRN-PC.fritz.box>
2016-04-28 14:58:33 +0200

    Test finish

---
+++ src/de/bht/fpa/mail/s819191/model/appLogic/placeholder.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bht.fpa.mail.s819191.model.appLogic;
+
+/**
+ *
+ * @author Admin
+ */
+public class placeholder {
+    
+}
