e13b3efbcc5b0d5a71ddd0ecb4fea3776ece4d7c
FerhanKaplanseren <FerhanKaplanseren@172.17.0.195>
2016-05-11 20:36:17 +0200

    Exercise 2 done.

--- src/de/bht/fpa/mail/s819191/controller/MainWindowController.java
+++ src/de/bht/fpa/mail/s819191/controller/MainWindowController.java
@@ -14,6 +14,12 @@ import de.bht.fpa.mail.s819191.model.data.Component;
 import de.bht.fpa.mail.s819191.model.data.FileElement;
 import de.bht.fpa.mail.s819191.model.data.Folder;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeItem.TreeModificationEvent;
 import javafx.scene.image.Image;
@@ -29,52 +35,67 @@ public class MainWindowController implements Initializable {
     private final File ROOT_PATH = new File(System.getProperty("user.home"));
     private final Image FOLDER_ICON = new Image("/de/bht/fpa/mail/s819191/icons/folder.png");
     private final Image FILE_ICON = new Image("/de/bht/fpa/mail/s819191/icons/file.png");
-    private TreeItem<Component> mainDir = new TreeItem<>(new Folder(ROOT_PATH, true), new ImageView(FOLDER_ICON));
+    private final TreeItem<Component> DUMMY = new TreeItem<>();
 
     @FXML
     private TreeView<Component> dirTree;
 
     private void configureTree() {
-        mainDir.setExpanded(true);
-        dirTree.setRoot(mainDir);
-        treeContent(mainDir);
-        mainDir.addEventHandler(TreeItem.branchExpandedEvent(), (TreeItem.TreeModificationEvent<Component> e) -> branchExpand(e));
-        mainDir.addEventHandler(TreeItem.branchCollapsedEvent(), (TreeItem.TreeModificationEvent<Component> e) -> branchCollapse(e));
+        final Folder MAIN_FOLDER = new Folder(ROOT_PATH, false);
+        final TreeItem<Component> mainDirTree = new TreeItem<>(MAIN_FOLDER, new ImageView(FOLDER_ICON));
+        showItems(MAIN_FOLDER, mainDirTree);
+        mainDirTree.setExpanded(true);
+        dirTree.setRoot(mainDirTree);
+        mainDirTree.addEventHandler(TreeItem.branchExpandedEvent(), (TreeItem.TreeModificationEvent<Component> e) -> branchExpand(e));
+        mainDirTree.addEventHandler(TreeItem.branchCollapsedEvent(), (TreeItem.TreeModificationEvent<Component> e) -> branchCollapse(e));
     }
 
-    public void branchExpand(TreeModificationEvent<Component> e) {
-        treeContent(e.getTreeItem());
+    public void branchExpand(final TreeModificationEvent<Component> e) {
+        showItems((Folder) e.getSource().getValue(), e.getTreeItem());
     }
 
-    private void branchCollapse(TreeModificationEvent<Component> e) {
+    private void branchCollapse(final TreeModificationEvent<Component> e) {
+        e.getTreeItem().getChildren().removeAll(e.getTreeItem().getChildren());
+        e.getTreeItem().getChildren().add(DUMMY);
     }
 
-    /**
-     * Builds the TreeView of given dir taken from a TreeItem and checks whether
-     * the loaded dir contains sub-directorys and/or files. If it contains dirs,
-     * it checks whether the dir is empty or not to whether show or not show the
-     * expandable Button next to the icon. Files will be showed without
-     * expandable Button.
-     *
-     * @param x a TreeItem
-     */
-    public void treeContent(TreeItem x) {
-        x.getChildren().clear();
-        File file = new File(((TreeItem<Component>) x).getValue().getPath());
-        for (File y : file.listFiles()) {
-            if (y.isDirectory()) {
-                if (y.list().length == 0) {
-                    TreeItem<Folder> z = new TreeItem<>(new Folder(y.getAbsoluteFile(), false), new ImageView(FOLDER_ICON));
-                    x.getChildren().add(z);
+    public ArrayList<Component> loadContent(final Folder f) {
+        final ArrayList<Component> al = new ArrayList<>();
+        final File file = new File(f.getPath());
+        for (File fi : file.listFiles()) {
+            if (fi.isDirectory()) {
+                if (fi.list().length == 0) {
+                    Folder y = new Folder(fi.getAbsoluteFile(), false);
+                    al.add(y);
                 } else {
-                    TreeItem<Component> z = new TreeItem<>(new Folder(y.getAbsoluteFile(), true), new ImageView(FOLDER_ICON));
-                    x.getChildren().add(z);
+                    Folder y = new Folder(fi.getAbsoluteFile(), true);
+                    al.add(y);
                 }
             } else {
-                TreeItem<Component> z = new TreeItem<>(new FileElement(y.getAbsoluteFile()), new ImageView(FILE_ICON));
-                x.getChildren().add(z);
+                FileElement y = new FileElement(fi.getAbsoluteFile());
+                al.add(y);
             }
         }
+        return al;
+    }
+
+    public void showItems(final Folder f, final TreeItem parent) {
+        loadContent(f).stream().forEach((com) -> {
+            File file = new File(com.getPath());
+            if (com.getClass().getName().contains("Folder")) {
+                if (com.isExpandable()) {
+                    TreeItem<Component> z = new TreeItem<>(new Folder(file, true), new ImageView(FOLDER_ICON));
+                    parent.getChildren().add(z);
+                    z.getChildren().add(DUMMY);
+                } else if (!com.isExpandable()) {
+                    TreeItem<Component> z = new TreeItem<>(new Folder(file, false), new ImageView(FOLDER_ICON));
+                    parent.getChildren().add(z);
+                }
+            } else {
+                TreeItem<Component> z = new TreeItem<>(new FileElement(file), new ImageView(FILE_ICON));
+                parent.getChildren().add(z);
+            }
+        });
     }
 
     /**
